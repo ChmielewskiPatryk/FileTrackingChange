497d2fe094cf5dc2fa1018b6ac5af113df36139b
Chmiel <pchmielewski338@gmail.com>
2022-02-17 18:18:31 +0100

    initial commit

---
+++ src/main/java/com/example/fileTrackingChange/FileTrackingChangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.fileTrackingChange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileTrackingChangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileTrackingChangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fileTrackingChange/FileTrackingChangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileTrackingChange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileTrackingChangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
